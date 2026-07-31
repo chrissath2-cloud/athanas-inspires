436cd5afc39b79f861b8b878948517c6468f625c
chrissath2-cloud <chriss.ath2@gmail.com>
2026-07-31 22:39:57 +0300

    Delete SET UP WEBSITE.bat

--- SET UP WEBSITE.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Athanas Inspires - First Time Setup
-echo Installing the website building tools...
-npm install
-if errorlevel 1 (echo. & echo SETUP FAILED. Read guides\TROUBLESHOOTING.md & pause & exit /b 1)
-echo.
-echo SETUP COMPLETE. You can now double-click UPDATE WEBSITE.bat.
-pause

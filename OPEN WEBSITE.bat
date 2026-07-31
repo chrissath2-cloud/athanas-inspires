21bd96fbf70e73a807704a960197996b6313c449
chrissath2-cloud <chriss.ath2@gmail.com>
2026-07-31 22:39:47 +0300

    Delete OPEN WEBSITE.bat

--- OPEN WEBSITE.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Athanas Inspires - Local Preview
-if not exist website\index.html (echo The website has not been built yet. Run UPDATE WEBSITE.bat first. & pause & exit /b 1)
-node scripts\preview.js
-pause
